957e02e588a9d71c39409876dd07a03e83bac90c
donhbk <awonloshi@gmail.com>
2023-10-26 13:47:35 +0200

    Update claude.py by changing description to french

--- lib/model-interfaces/langchain/functions/request-handler/adapters/bedrock/claude.py
+++ lib/model-interfaces/langchain/functions/request-handler/adapters/bedrock/claude.py
@@ -35,7 +35,7 @@ class BedrockClaudeAdapter(ModelAdapter):
     def get_qa_prompt(self):
         template = """
 
-Human: Use the following pieces of context to answer the question at the end. If you don't know the answer, just say that you don't know, don't try to make up an answer.
+Human: Utilise les éléments de contexte suivants pour répondre à la question finale. Si tu ne connais pas la réponse, dis simplement que tu ne sais pas, n'essaie pas d'inventer une réponse.
 
 {context}
 
@@ -50,7 +50,7 @@ Assistant:"""
     def get_prompt(self):
         template = """
 
-Human: The following is a friendly conversation between a human and an AI. If the AI does not know the answer to a question, it truthfully says it does not know.
+Human: Ce qui suit est une conversation amicale entre un humain et une IA. Si l'IA ne connaît pas la réponse à une question, elle dit sincèrement qu'elle ne sait pas.
 
 Current conversation:
 {chat_history}
@@ -73,7 +73,7 @@ Assistant:"""
         template = """
 {chat_history}
 
-Human: Given the following conversation and a follow up question, rephrase the follow up question to be a standalone question, in its original language.
+Human: A partir de la conversation suivante et d'une question de follow-up, reformulez la question de suivi pour en faire une question à part entière, dans sa langue d'origine.
 Follow Up Input: {question}
 
 Assistant:"""
